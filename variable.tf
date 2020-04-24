4401f5fd47fe71511f8aef6117d21f8f814e0a7e
saikumar-123 <doppalapudisaikumar@gmail.com>
2020-04-24 03:00:17 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "security_groups" {
   description = " security groups"
   type = "map"
-  default = { security_groups = "sg-0884d2376ed8f2dd3,sg-0707e89aaefb0816a" }
+  default = { security_groups = "sg-0884d2xxxxxxxx,sg-0707e89aaxxxxxxxx" }
 }
